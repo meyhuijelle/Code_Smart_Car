bb0f41162a5295a01ecb22ac04811b7e18a86001
Diero <dieter.roobrouck@howest.be>
2021-06-15 10:07:29 +0200

    code_cleaned

--- Project_smart_car/Backend/repositories/DataRepository.py
+++ Project_smart_car/Backend/repositories/DataRepository.py
@@ -10,11 +10,13 @@ class DataRepository:
             gegevens = request.form.to_dict()
         return gegevens
 
+    # Read the history table of the database.
     @staticmethod
     def read_historiek():
         sql = "SELECT * FROM devicesdb.historiek ORDER BY HistoriekID DESC LIMIT 10"
         return Database.get_rows(sql)
 
+    # Create a history for in the database.
     @staticmethod
     def create_historiek(DeviceID, ActieID, Actiedatum, Waarde, Commentaar):
         sql = "INSERT INTO devicesdb.historiek(DeviceID,ActieID,Actiedatum, Waarde, Commentaar) VALUES(%s,%s,%s,%s,%s)"
